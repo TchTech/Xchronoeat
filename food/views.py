abb8d78fe15a59cdfac707f78de8e5d2d50335fe
TchTech <67865526+TchTech@users.noreply.github.com>
2022-09-10 21:21:23 +0300

    frontend fix

--- food/views.py
+++ food/views.py
@@ -1,9 +1,12 @@
+from turtle import title
 from django.shortcuts import render
 from .models import CommonFood
 from django.db.models import Q
 import json
 import os
 from pathlib import Path
+
+APP_TITLE = "ИнфоЕда"
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -15,15 +18,15 @@ with open(os.path.join(os.path.dirname(BASE_DIR), "food", "label_to_image.json")
 print(image_to_label)
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {"title": APP_TITLE})
 
 def search(request):
     query = request.GET.get("query")
     if query == "" or query == None:
-        return render(request, "search.html", {"query": "", "foodLength":0, "searchedItems":{}})
+        return render(request, "search.html", {"query": "", "foodLength":0, "searchedItems":{}, "title":APP_TITLE})
     else:
         objects = CommonFood.objects.filter(name__icontains=query).values()
         for i in range(objects.__len__()):
             objects[i]["image"] = "food/img/comfood/"+image_to_label[objects[i]["category"]]
             
-        return render(request, "search.html", {"query": query, "foodLength":objects.__len__(), "searchedItems":objects})
+        return render(request, "search.html", {"query": query, "foodLength":objects.__len__(), "searchedItems":objects, "title":APP_TITLE})
